edfa68521e8047e64a9db303c33536f002b77ba7
Watnu03 <watnu03@gmail.com>
2025-01-01 03:06:45 +0900

    Feat: 유저 상태 갱신 처리 추가

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,7 +1,19 @@
-import { Outlet } from "react-router-dom";
 import Header from "./Header";
+import { Outlet } from "react-router-dom";
+import useAuth from "../../hooks/useAuth";
+import { useEffect } from "react";
 
 const Layout = () => {
+  const { checkAuthStatus, isLoading } = useAuth();
+
+  useEffect(() => {
+    checkAuthStatus();
+  }, [checkAuthStatus]);
+
+  if (isLoading) {
+    return <div>Loading...</div>; // 로딩페이지 자리
+  }
+
   return (
     <>
       <Header />
