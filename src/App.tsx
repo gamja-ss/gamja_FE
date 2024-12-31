16a7e643622d62b154aeb841a9d6aa9d455850e7
Watnu03 <watnu03@gmail.com>
2025-01-01 03:03:57 +0900

    Feat: 로그인 및 Callback 라우트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+
+import Callback from "./page/auth/Callback";
 import Home from "./page/home/Home";
 import Inventory from "./page/inventory/Inventory";
 import Layout from "./components/layout/Layout";
+import Login from "./page/auth/Login";
 
 function App() {
   return (
     <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/auth/login/callback" element={<Callback />} />
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/inventory" element={<Navigate to="/inventory/closet" replace />} />
