345b4345217019fbb3379386f760607476c9fddb
h22jul22 <164333745+h22jul22@users.noreply.github.com>
2025-01-03 18:26:30 +0900

    Remove: src/utils/dateUtils.ts 파일 삭제

--- src/utils/dateUtils.ts
+++
@@ -1,7 +0,0 @@
-export const formatDate = (dateString: Date) => {
-  return new Date(dateString).toLocaleDateString("ko-KR", {
-    year: "numeric",
-    month: "numeric",
-    day: "numeric",
-  });
-};
